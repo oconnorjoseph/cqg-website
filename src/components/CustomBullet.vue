490782f698d79f96eaf477b558e4e6ea5b236780
Joseph O'Connor <oconnor.joseph@columbia.edu>
2020-03-01 10:36:29 -0500

    Jucification stepping stones for Eilam

---
+++ src/components/CustomBullet.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    :id="this.$options.name"
+    class="row align-items-center mb-2"
+    @mouseenter="onHoverOn()"
+    @mouseleave="onHoverOut()"
+  >
+    <div class="col-auto p-0" style="min-width: 50px;">
+      <slot name="bullet" v-bind="{ colorHoverable }" />
+    </div>
+    <div class="col pl-1">
+      <p class="text-primary text-left mb-0 text-highlight">
+        <slot />
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CustomBullet",
+  data() {
+    return {
+      color: "#7088a0",
+      colorHover: "#003366",
+      colorHoverable: "#7088a0"
+    };
+  },
+  methods: {
+    onHoverOn() {
+      this.colorHoverable = this.colorHover;
+    },
+    onHoverOut() {
+      this.colorHoverable = this.color;
+    }
+  }
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,59 @@
 <template>
   <div :id="this.$options.name">
+    <div class="row mx-2 mb-4">
+      <header class="mx-auto text-center slide-in-out">
+        <div class="pb-2">
+          <logo-svg class="logo-view" />
+        </div>
+        <h5 class="font-weight-light">
+          We strive to facilitate a community of quantitative thinking.
+        </h5>
+      </header>
+    </div>
+    <div class="row m-2">
+      <header class="mx-auto text-center m-4 header-width">
+        <div class="pb-2">
+          <h1 class="scale-display-4">Who We Are</h1>
+        </div>
+        <custom-bullet>
+          <template #bullet="{ colorHoverable }">
+            <rocket-svg :color="colorHoverable" />
+          </template>
+          <template #default>
+            Columbia Quant Group (CQG) is Columbia's first and only Quantitative
+            Finance organization.
+          </template>
+        </custom-bullet>
+        <custom-bullet>
+          <template #bullet="{ colorHoverable }">
+            <puzzle-svg :color="colorHoverable" />
+          </template>
+          <template #default>
+            We aim to ignite students' creativity and problem-solving skills by
+            connecting them with real-world professionals and introducing them
+            to modern quantitative analysis.
+          </template>
+        </custom-bullet>
+        <custom-bullet>
+          <template #bullet="{ colorHoverable }">
+            <crowd-svg :color="colorHoverable" />
+          </template>
+          <template #default>
+            Membership is open for all Columbia University Students. We
+            currently have more than 250 members across Columbia's undergraduate
+            and graduate schools.
+          </template>
+        </custom-bullet>
+      </header>
+    </div>
     <div class="row m-2">
       <header class="mx-auto text-center m-4">
         <div class="pb-2">
           <h1 class="scale-display-4">Executive Board</h1>
         </div>
-        <h5 class="font-weight-light">
+        <!-- <h5 class="font-weight-light">
           We strive to facilitate a community of quantitative thinking.
-        </h5>
+        </h5> -->
       </header>
     </div>
     <member-board :group="0" />
@@ -19,7 +65,17 @@ export default {
   name: "About",
   components: {
     MemberBoard: () =>
-      import(/* webpackPreload: true */ "../components/MemberBoard.vue")
+      import(/* webpackPreload: true */ "../components/MemberBoard.vue"),
+    CustomBullet: () =>
+      import(/* webpackPreload: true */ "../components/CustomBullet.vue"),
+    LogoSvg: () =>
+      import(/* webpackPreload: true */ "../components/svg/LogoSvg.vue"),
+    RocketSvg: () =>
+      import(/* webpackPreload: true */ "../components/svg/RocketSvg.vue"),
+    PuzzleSvg: () =>
+      import(/* webpackPreload: true */ "../components/svg/PuzzleSvg.vue"),
+    CrowdSvg: () =>
+      import(/* webpackPreload: true */ "../components/svg/CrowdSvg.vue")
   }
 };
 </script>
